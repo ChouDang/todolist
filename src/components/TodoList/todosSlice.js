45019b8e1c24f79b297ea4985a8a82ce95394900
ChouDang <hoaitrongdang@gmail.com>
2022-08-04 16:52:20 +0700

    update todo redux

--- src/components/TodoList/todosSlice.js
+++ src/components/TodoList/todosSlice.js
@@ -1,45 +1,77 @@
-// const initState = [
-//   { id: 1, name: 'Learn Yoga', completed: false, priority: 'Medium' },
-//   { id: 2, name: 'Learn Redux', completed: true, priority: 'High' },
-//   { id: 3, name: 'Learn JavaScript', completed: false, priority: 'Low' },
-// ];
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-// const todoListReducer = (state = initState, action) => {
-//   switch (action.type) {
-//     case 'todoList/addTodo':
-//       return [...state, action.payload];
-
-//     case 'todoList/toggleTodoStatus':
-//       return state.map((todo) =>
-//         todo.id === action.payload
-//           ? { ...todo, completed: !todo.completed }
-//           : todo
-//       );
-//     default:
-//       return state;
-//   }
-// };
-
-// export default todoListReducer;
-
-import { createSlice } from '@reduxjs/toolkit';
-
-export default createSlice({
-  name: 'todoList',
-  initialState: [
-    { id: 1, name: 'Learn Yoga', completed: false, priority: 'Medium' },
-    { id: 2, name: 'Learn Redux', completed: true, priority: 'High' },
-    { id: 3, name: 'Learn JavaScript', completed: false, priority: 'Low' },
-  ],
-  reducers: { // IMMER
-    addTodo: (state, action) => {
-      state.push(action.payload);
-    }, // action creators
-    toggleTodoStatus: (state, action) => {
-      const currentTodo = state.find(todo => todo.id === action.payload);
-      if (currentTodo) {
-        currentTodo.completed = !currentTodo.completed;
-      }
-    }
-  }
+const todosSlice = createSlice({
+    name: 'todoList',
+    initialState: { status: 'idle', todos: [] },
+    reducers: {
+        // IMMER
+        addTodo: (state, action) => {
+            state.push(action.payload);
+        }, // action creators
+        toggleTodoStatus: (state, action) => {
+            const currentTodo = state.find((todo) => todo.id === action.payload);
+            if (currentTodo) {
+                currentTodo.completed = !currentTodo.completed;
+            }
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchTodos.pending, (state, action) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchTodos.fulfilled, (state, action) => {
+                console.log({ action });
+                state.todos = action.payload;
+                state.status = 'idle';
+            })
+            .addCase(addNewTodo.fulfilled, (state, action) => {
+                state.todos.push(action.payload);
+            })
+            .addCase(updateTodo.fulfilled, (state, action) => {
+                let currentTodo = state.todos.find((todo) => todo.id === action.payload);
+                currentTodo = action.payload;
+            });
+    },
 });
+
+export const fetchTodos = createAsyncThunk('todos/fetchTodos', async () => {
+    const res = await fetch('/api/todos');
+    const data = await res.json();
+    return data.todos;
+});
+
+export const addNewTodo = createAsyncThunk(
+    'todos/addNewTodo',
+    async (newTodo) => {
+        const res = await fetch('/api/todos', {
+            method: 'POST',
+            body: JSON.stringify(newTodo),
+        });
+        const data = await res.json();
+        console.log({ data });
+        return data.todos;
+    }
+);
+
+export const updateTodo = createAsyncThunk(
+    'todos/updateTodo',
+    async (updatedTodo) => {
+        const res = await fetch('/api/updateTodo', {
+            method: 'POST',
+            body: JSON.stringify(updatedTodo),
+        });
+
+        const data = await res.json();
+        console.log('[updateTodo]', { data })
+        return data.todos;
+    }
+);
+
+/*
+  => todos/fetchTodos/pending
+  => todos/fetchTodos/fullfilled
+  => todos/fetchTodos/rejected
+*/
+
+export default todosSlice;
